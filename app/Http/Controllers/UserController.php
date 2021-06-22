967c31a553f4b7bfc611a4aa5aa8e4a83013581d
Miecaela Garcia <76048114+mvsgarcia@users.noreply.github.com>
2021-06-22 15:53:30 +0800

    version 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserController extends Controller
@@ -61,7 +62,25 @@ class UserController extends Controller
     // }
     public function store(Request $request)
     {
-        User::create($request->all());
+        
+        $user=User::create($request->all());
+
+        $user->password = Hash::make($request->password);
+
+        $user->fill($request->except('password'))->save();
+
+
+
+        // $user=User::create($request->except('password'));
+
+        // if($request->has('password')){
+
+        //    $user->password = Hash::make($request->password);
+        // };
+
+
+        
+
         return redirect()->route('users.index');
     } 
 
@@ -122,9 +141,17 @@ class UserController extends Controller
     public function update(Request $request, int $id)
     {
         $user = User::findOrFail($id);
-        $user->update($request->all());
+        
+        
+        if($request->has('password')){
+           $user->password=Hash::make($request->password);
+        };
 
-     
+        $user->fill($request->except('password'))->save();
+        //$user->fill($request->all())->save();
+
+        
+
         return redirect()->route('users.index');
     }
     //     public function update(Request $request, User $user)
